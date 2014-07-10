5e0fceb739a7a42e8572dc09807198d07f30ef1a
Eric Mashlan <eric.mashlan@mantis-tgi.com>
2014-07-10 09:18:46 -0700

    update to css

--- Diggity.Web/App_Start/BundleConfig.cs
+++ Diggity.Web/App_Start/BundleConfig.cs
@@ -8,9 +8,7 @@ namespace Diggity.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/vendors/jquery/jquery-{version}.js",
-                "~/Scripts/vendors/jquery/jquery-ui.multidatepicker.js",
-                "~/Scripts/vendors/jquery/jquery.customInput.js"));
+                "~/Scripts/vendors/jquery/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -19,9 +17,7 @@ namespace Diggity.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/vendors/bootstrap/bootstrap.js",
-                "~/Scripts/vendors/respond/respond.js",
-                "~/Scripts/vendors/bootstrap/general.js",
-                "~/Scripts/vendors/bootstrap/custom.js"));
+                "~/Scripts/vendors/respond/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
                 "~/Scripts/vendors/angularjs/i18n/angular-locale_en_us.js",
@@ -48,7 +44,7 @@ namespace Diggity.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
-                "~/Content/style.css",
+                "~/Content/bootstrap-theme.css",
                 "~/Content/Site.css",
                 "~/Content/font-awesome.css"));
 
