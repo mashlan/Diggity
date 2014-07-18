b977af085cfe75b879aac62f32680cdf63226815
Eric Mashlan <eric.mashlan@mantis-tgi.com>
2014-07-18 11:17:23 -0700

    added login info

---
+++ Diggity.Web/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Diggity.Services;
+
+namespace Diggity.Web.Controllers
+{
+    public class LoginController : BaseController
+    {
+        // GET: Login
+        public LoginController(IServiceAggregate serviceAggregate) : base(serviceAggregate)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
