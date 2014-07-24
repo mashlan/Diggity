7a5ca013342591b29d6049c9b9ac7a7faa50141f
Eric Mashlan <eric.mashlan@mantis-tgi.com>
2014-07-24 14:01:22 -0700

    updates to timer

--- Diggity.Web/Scripts/controllers/timerController.js
+++ Diggity.Web/Scripts/controllers/timerController.js
@@ -4,94 +4,226 @@
 myControllers.controller('TimerCtrl', ['$routeParams', '$scope', '$location',
     function ($routeParams, $scope, $location) {
         'use strict';
-        $scope.timer = {
-            minute: '00',
-            second: '00',
-            isCountDown: false
-        };
 
+        $scope.isCountDown = false;
         $scope.lessThanTen = false;
+        $scope.Minutes = {
+            TenSpot: createNumberArray(9, "min_tens"),
+            OneSpot: createNumberArray(9, "min_ones")
+        };
+        $scope.Seconds = {
+            TenSpot: createNumberArray(5, "sec_tens"),
+            OneSpot: createNumberArray(9, "sec_ones")
+        }
 
         var myTimer;
+        var scrollMargin = 364;
+        var minTenUl = $("#minutes_tens");
+        var minOneUl = $("#minutes_ones");
+        var secTenUl = $("#seconds_tens");
+        var secOneUl = $("#seconds_ones");
+
+        $(minTenUl).bind("mousewheel", function (e) {
+            wheel(minTenUl, $scope.Minutes.TenSpot, e);
+        });
+
+        $(minOneUl).bind("mousewheel", function (e) {
+            wheel(minOneUl, $scope.Minutes.OneSpot, e);
+        });
+
+        $(secTenUl).bind("mousewheel", function (e) {
+            wheel(secTenUl, $scope.Seconds.TenSpot, e);
+        });
+
+        $(secOneUl).bind("mousewheel", function (e) {
+            wheel(secOneUl, $scope.Seconds.OneSpot, e);
+        });
+
+        function wheel(element, array, event) {
+            var moveUp = (event.originalEvent.wheelDelta >= 0);
+            event.preventDefault();
+
+            $.each(array, function (i, v) {
+                if (v.isSelected) {
+                    v.isSelected = false;
+                    var index = moveUp ? i - 1 : i + 1;
+
+                    if (index < 0) index = 0;
+                    if (index >= array.length) index = array.length - 1;
+
+                    array[index].isSelected = true;
+                    element.animate({
+                        scrollTop: scrollMargin * index
+                    }, 100);
+                    return false;
+                }
+            });
+        }
+
 
         $scope.stopTimer = function() {
             window.clearInterval(myTimer);
         }
 
         $scope.startTimer = function() {
-            myTimer = setInterval(function () { myTimerFunc(); }, 1000);
-            var second = parseInt($scope.timer.second);
-            var minute = parseInt($scope.timer.minute);
-            $scope.lessThanTen = false;
+            var seconds = getSeconds();
+            var minutes = getMinutes();
             var beep = document.getElementById("beep_sound");
 
-            function myTimerFunc() {
-                if ($scope.timer.isCountDown) {
-                    second--;
-                    if (second < 0) {
-                        if (minute > 0) {
-                            minute--;
-                            if (minute >= 0) {
-                                second = 59;
-                            }
-                        }
-                    }
-                    $scope.lessThanTen = (minute < 1 && second < 11);
-                    if ($scope.lessThanTen) beep.play();
+            var startingSeconds = seconds;
+            var startingMinutes = minutes;
 
+            var d0 = (new Date());
+            d0 = (new Date(d0.getTime() + (minutes * 60000) + (seconds * 1000))).valueOf();
+            myTimer = setInterval(function() { myTimerFunc(d0); }, 1000);
+
+            function myTimerFunc(initialTime) {
+                var now = new Date();
+                if ($scope.isCountDown) {
+                    now = now.valueOf();
                 } else {
-                    if (second == 59) {
-                        minute++;
-                        second = 0;
-                    } else {
-                        second++;
-                    }
+                    now = (new Date(now.getTime() + (startingMinutes * 60000) + (startingSeconds * 1000))).valueOf();
                 }
-                setSecond(second);
-                setMinute(minute);
-                $scope.$apply();
 
-                if (second + minute === 0) window.clearInterval(myTimer);
+                // calculate time difference between now and initial time
+                var diff = Math.abs(now - initialTime);
 
+                //if we are not counting down, add the set starting time
+                if (!$scope.isCountDown) {
+                    diff = diff + (startingMinutes * 60000) + (startingSeconds * 1000);
+                }
+
+                minutes = Math.floor(diff / 1000 / 60);
+                seconds = Math.floor(diff / 1000) - minutes*60;
+                
+                var secOnes = seconds % 10;
+                var secTens = Math.floor(seconds / 10);
+                var minOnes = minutes % 10;
+                var minTens = Math.floor(minutes / 10);
+
+                triggerAnimation(minTens, minOnes, secTens, secOnes);
+
+                if ($scope.isCountDown) {
+                    $scope.lessThanTen = (minutes < 1 && seconds < 10);
+                    $scope.$apply();
+                    if ($scope.lessThanTen && $scope.isCountDown) beep.play();
+                }
+
+                if (seconds === 0 && minutes === 0) {
+                    window.clearInterval(myTimer);
+                }
+
+                updateSelectedTime(secOnes, secTens, minOnes, minTens);
             }
+
         }
 
-        $scope.resetTimer = function() {
-            $scope.timer.minute = "00";
-            $scope.timer.second = "00";
+        $scope.resetTimer = function () {
             $scope.lessThanTen = false;
+            window.clearInterval(myTimer);
+            updateSelectedTime(0, 0, 0, 0);
+            triggerAnimation(0, 0, 0, 0);
         }
 
-        $scope.increaseMinute = function() {
-            var min = parseInt($scope.timer.minute);
-            min++;
-            setMinute(min);
+        function triggerAnimation(minTens, minOnes, secTens, secOnes) {
+            minTenUl.animate({
+                scrollTop: scrollMargin * (minTens)
+            }, 200);
+
+            minOneUl.animate({
+                scrollTop: scrollMargin * (minOnes)
+            }, 200);
+
+            secTenUl.animate({
+                scrollTop: scrollMargin * (secTens)
+            }, 200);
+
+            secOneUl.animate({
+                scrollTop: scrollMargin * (secOnes)
+            }, 200);
         }
 
-        $scope.decreaseMinute = function () {
-            var min = parseInt($scope.timer.minute);
-            min = min === 0 ? 0 : min - 1;
-            setMinute(min);
+        function getSeconds() {
+            var tensValue = $.grep($scope.Seconds.TenSpot, function (v) { return v.isSelected; })[0].value;
+            var onesValue = $.grep($scope.Seconds.OneSpot, function (v) { return v.isSelected; })[0].value;
+
+            return (tensValue * 10) + onesValue;
         }
 
-        $scope.increaseSecond = function() {
-            var sec = parseInt($scope.timer.second);
-            sec++;
-            setSecond(sec);
+        function getMinutes() {
+            var tensValue = $.grep($scope.Minutes.TenSpot, function (v) { return v.isSelected; })[0].value;
+            var onesValue = $.grep($scope.Minutes.OneSpot, function (v) { return v.isSelected; })[0].value;
+
+            return (tensValue * 10) + onesValue;
         }
 
-        $scope.decreaseSecond = function() {
-            var sec = parseInt($scope.timer.second);
-            sec = sec === 0 ? 0 : sec - 1;
-            setSecond(sec);
+        function updateSelectedTime(secOnes, secTens, minOnes, minTens) {
+            resetSelected();
+            $scope.Minutes.TenSpot[minTens].isSelected = true;
+            $scope.Minutes.OneSpot[minOnes].isSelected = true;
+            $scope.Seconds.TenSpot[secTens].isSelected = true;
+            $scope.Seconds.OneSpot[secOnes].isSelected = true;
         }
 
-        function setSecond(second) {
-            $scope.timer.second = second < 10 ? "0" + second : second.toString();
+        function resetSelected() {
+            $.grep($scope.Seconds.TenSpot, function (v) { return v.isSelected; })[0].isSelected = false;
+            $.grep($scope.Seconds.OneSpot, function (v) { return v.isSelected; })[0].isSelected = false;
+            $.grep($scope.Minutes.TenSpot, function (v) { return v.isSelected; })[0].isSelected = false;
+            $.grep($scope.Minutes.OneSpot, function (v) { return v.isSelected; })[0].isSelected = false;
         }
 
-        function setMinute(minute) {
-            $scope.timer.minute = minute < 10 ? "0" + minute : minute.toString();
+
+        function createNumberArray(topNumber, id) {
+            var numbers = [];
+
+            for (var i = 0; i <= topNumber; i++) {
+                numbers.push({ value: i, isSelected: (i === 0), id: id + "_" + i });
+            }
+            return numbers;
+        }
+
+        $scope.changeTime = function (array, id) {
+            var parent = $("#" + id).parent();
+
+            $.each(array, function(i, v) {
+                if (v.isSelected && v.value < array.length -1) {
+                    v.isSelected = false;
+                    array[i + 1].isSelected = true;
+                    parent.animate({
+                        scrollTop: scrollMargin * (v.value + 1)
+                    }, 300);
+                    return false;
+                }
+            });
+
+
+
+
+            //var $this = $(element);
+
+            ////get currently selected
+            //var selected = parseInt($this.find('.selected').text());
+
+            ////if (selected === 9 && !moveup) return;
+
+            //if (moveup) {
+            //    selected = selected === 0 ? 0 : selected - 1;
+            //} else {
+            //    selected++;
+            //}
+
+            ////clear classes
+            //$this.children().removeClass('selected');
+            //var child = $this.find('li:contains(' + selected + ')');
+
+            //if (child.length > 0) {
+            //    child.addClass('selected');
+            //} else {
+            //    selected = moveup ? selected + 1 : selected - 1;
+            //}
+
+            
+
         }
     }
 ]);
